47a51bf6b2200a93dc5d3cebb5c803b1a0ea2d46
Daniel Minichetti <daniel.minichetti@gmail.com>
2025-03-20 20:56:22 -0700

    CV Download & Project Links
    Added some initial projects that are linked to their respective directories/repos as well as added a button to download my latest cv (would like the button to be more animated but I'll wait)

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -33,7 +33,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://www.ceias.nau.edu/capstone/projects/CS/2024/DiverseMakers_S24/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -70,7 +70,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/Daniel-Minichetti/ProjectPinecone"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -86,13 +86,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <h3 className="text-xl font-bold mb-2">TBD</h3>
               <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
+                /*add text here*/
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
+                {[""].map(
                   (tech) => (
                     <span
                       key={tech}
@@ -127,13 +126,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">TBD</h3>
               <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
+                /*add text here*/
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                {[""].map((tech, key) => (
                   <span
                     key={key}
                     className="
